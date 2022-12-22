dbe5428e18840f7570ca693c8db18b6d5cc3125f
wufeisheng <wfs1997@163.com>
2022-12-22 11:58:40 +0000

    update main func

--- main.cc
+++ main.cc
@@ -2,12 +2,19 @@
 
 int main() {
     int k = 12288, n = 4608;
-    std::vector<int> mm{16, 16*128};
-    std::vector<int> kk{4096};
-    std::vector<int> nn{1024};
+    std::vector<int> mm{1, 2, 4, 8, 16, 32};
+    auto mm_tmp = std::vector<int>(mm);
+    int seq_len = 128;
+    for (auto& m : mm) {
+        mm.push_back(m * seq_len);
+        m *= seq_len;
+    }
+    std::vector<int> kk{384, 384, 1536, 384}; //, 12288, 1536, 12288, 6144};
+    std::vector<int> nn{1152, 384, 384, 1536} ;//, 4096, 12288, 6144, 12288};
     for (auto m : mm) {
-        for (auto k : kk) {
-            for (auto n : nn) {
+        for (int i = 0; i < kk.size(); ++i) {
+                n = nn[i];
+                k = kk[i];
                 auto A = std::vector<int8_t>(m * k);
                 auto B = std::vector<int8_t>(k * n);
                 auto C = std::vector<int32_t>(m * n);
@@ -15,7 +22,6 @@ int main() {
                 ct::CUBLASLTContext dev_ctx;
 
                 ct::GEMM(dev_ctx, A,B,C, m,k,n,true);
-            }
         }
     }
 }
